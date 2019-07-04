98a4a650e402a26db4a59c4fde9bb8249163ed37
- <->
2019-07-04 18:32:38 +0800

    .

--- bls/bls.go
+++ bls/bls.go
@@ -9,8 +9,19 @@ import (
 	"github.com/u6du/key"
 )
 
+type PrivateKey []byte
+type PublicKey []byte
+
 // https://github.com/prysmaticlabs/prysm/blob/master/shared/bls/bls.go
-var Private g1pubs.SecretKey
+var Private PrivateKey
+
+func (priv PrivateKey) Public() PublicKey {
+	var r [32]byte
+	copy(r[:], priv)
+	secret := g1pubs.DeserializeSecretKey(r)
+	t := g1pubs.PrivToPub(secret).Serialize()
+	return t[:]
+}
 
 func InitBls() {
 
@@ -26,9 +37,7 @@ func InitBls() {
 			return t[:], public[:]
 		})
 
-	var r [32]byte
-	copy(r[:], binary)
-	Private = *g1pubs.DeserializeSecretKey(r)
+	Private = binary
 }
 
 func init() {
